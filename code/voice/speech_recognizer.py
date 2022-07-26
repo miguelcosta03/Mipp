00c4b2299e9a3f03cb88fb0b8e6bac3d95d1622a
miguelcosta03 <myserofficial@gmail.com>
2022-07-26 12:52:12 +0100

    - Created new voice feature.
    - Created new voice feature: "speech recognition".

---
+++ code/voice/speech_recognizer.py
@@ -0,0 +1,20 @@
+import speech_recognition as sr
+
+
+class SpeechRecognizer:
+    def __init__(self):
+        recognizer = sr.Recognizer()
+        self.recognizer = recognizer
+
+    def recognize(self):
+        with sr.Microphone() as microphone:
+            print('Speak:')
+            audio = self.recognizer.listen(microphone)
+        try:
+            print(f'You said: {self.recognizer.recognize_google(audio, language="en")}')
+        except sr.UnknownValueError:
+            print('Could not understand what you said.')
+
+
+sp = SpeechRecognizer()
+sp.recognize()
